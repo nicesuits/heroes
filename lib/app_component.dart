3e658c85be4bcb163a4a32704e145a24ff1a4c10
Raion314 <raion314@outlook.com>
2019-01-07 19:43:20 -0800

    display all heroes

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,14 +2,15 @@ import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
 
 import './src/hero_model.dart';
+import './src/mock_heroes.dart';
 
 @Component(
   selector: 'my-app',
   styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: [formDirectives],
+  directives: [coreDirectives, formDirectives],
 )
 class AppComponent {
   final title = 'Tutorial of the Heroes';
-  Hero hero = Hero(1, 'Windstorm');
+  List<Hero> heroes = mockHeroes;
 }
