5132d546153cac4c704671210ff3104af39ecad8
Raion314 <raion314@outlook.com>
2019-01-07 20:44:40 -0800

    refactor app with child component

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,14 +1,14 @@
 import 'package:angular/angular.dart';
-import 'package:angular_forms/angular_forms.dart';
 
 import './src/hero_model.dart';
 import './src/mock_heroes.dart';
+import './src/hero/hero_component.dart';
 
 @Component(
   selector: 'my-app',
   styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: [coreDirectives, formDirectives],
+  directives: [coreDirectives, HeroComponent],
 )
 class AppComponent {
   final title = 'Tutorial of the Heroes';
