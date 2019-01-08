a4c33156b680b132ef9c58f395ef5175fe2fa4e5
Raion314 <raion314@outlook.com>
2019-01-07 19:36:54 -0800

    update template views

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,16 +1,15 @@
 import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
 
-import 'src/todo_list/todo_list_component.dart';
-
-// AngularDart info: https://webdev.dartlang.org/angular
-// Components info: https://webdev.dartlang.org/components
+import './src/hero_model.dart';
 
 @Component(
   selector: 'my-app',
   styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: [TodoListComponent],
+  directives: [formDirectives],
 )
 class AppComponent {
-  // Nothing here yet. All logic is in TodoListComponent.
+  final title = 'Tutorial of the Heroes';
+  Hero hero = Hero(1, 'Windstorm');
 }
