1c2f09f0f527094db02d074f1bf44866d558325d
Raion314 <raion314@outlook.com>
2019-01-09 21:08:41 -0800

    update to use http

--- lib/src/hero_service.dart
+++ lib/src/hero_service.dart
@@ -1,10 +1,34 @@
 import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart';
 
 import './hero_model.dart';
-import './mock_heroes.dart';
 
 class HeroService {
-  Future<List<Hero>> getAll() async => mockHeroes;
+  static const _heroesUrl = 'api/heroes';
+  final Client _http;
+
+  HeroService(this._http);
+
+  Future<List<Hero>> getAll() async {
+    try {
+      final response = await _http.get(_heroesUrl);
+      final heroes = (_extractData(response) as List)
+          .map((json) => Hero.fromJson(json))
+          .toList();
+      return heroes;
+    } catch (e) {
+      throw _handleError(e);
+    }
+  }
+
+  dynamic _extractData(Response response) => json.decode(response.body)['data'];
+
+  Exception _handleError(dynamic e) {
+    print(e);
+    return Exception('Server error; cause: $e');
+  }
 
   Future<Hero> get(int id) async =>
       (await getAll()).firstWhere((hero) => hero.id == id);
