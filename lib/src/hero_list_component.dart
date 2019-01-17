bd52695a39881e2a8091bc30abeb0e0a06d383d4
Raion314 <raion314@outlook.com>
2019-01-16 20:20:35 -0800

    add angular router

---
+++ lib/src/hero_list_component.dart
@@ -0,0 +1,10 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'my-heroes',
+  template: '''
+  <h2>Heroes</h2>
+  <p>Get your heroes here</p>
+  ''',
+)
+class HeroListComponent {}
